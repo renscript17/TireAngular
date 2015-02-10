b25352bc140a36fa657883158dc16d63ce55ad0e
RLAWRENCE <xyber_renz1211@live.com>
2015-02-10 23:12:20 +0800

    tire controller update

--- App/controllers/tireController.js
+++ App/controllers/tireController.js
@@ -1,8 +1,9 @@
 ﻿'use strict';
 
 var removeTemplate = '<input type="button" value="remove" ng-click="removeRow($index)" />';
-app.controller('tireController', function ($scope, $timeout) {
-    $scope.myData = [{ id: 7, brand: "Good Year", description: "GoodYear Excellence", quantity: 4, addons: "GoodYear mags" },
+app.controller('tireController',  function ($scope, $http, $timeout) {
+
+    $scope.myData = [{ brand: "Good Year", description: "GoodYear Excellence", quantity: 4, addons: "GoodYear mags" },
     ];
 
     $scope.gridOptions = {
@@ -11,7 +12,7 @@ app.controller('tireController', function ($scope, $timeout) {
         enableCellSelection: true,
         enableRowSelection: false,
         enableCellEditOnFocus: true,
-        columnDefs: [{ field: 'id', displayName: 'id', enableCellEdit: false }, { field: 'brand', displayName: 'brand', width: 120, enableCellEdit: true }, { field: 'description', displayName: 'description', width: 120, enableCellEdit: true },
+        columnDefs: [{ field: 'brand', displayName: 'brand', width: 120, enableCellEdit: true }, { field: 'description', displayName: 'description', width: 120, enableCellEdit: true },
             { field: 'quantity', displayName: 'quantity', enableCellEdit: true }, { field: 'addons', displayName: 'addons', width: 120, enableCellEdit: false }, { field: 'remove', displayName: '', cellTemplate: removeTemplate }]
     };
 
@@ -19,6 +20,26 @@ app.controller('tireController', function ($scope, $timeout) {
         var index = this.row.rowIndex;
         $scope.gridOptions.selectItem(index, false);
         $scope.myData.splice(index, 1);
+    };
+
+    $scope.tire = {};
+
+    $scope.submitData = function (tire, resultVarName) {
+
+        $http.post("../api/FormSubmit", tire)
+          .success(function (data) {
+              $scope.myData.push(
+                  { brand: data.brand, description: data.description, quantity: data.quantity, addons: data.addons }
+              );
+
+              $scope.gridOptions.data($scope.myData);
+
+          })
+          .error(function (data, status, headers) {
+              $scope[resultVarName] = "SUBMIT ERROR";
+        });
+
+
     };
 
 });
